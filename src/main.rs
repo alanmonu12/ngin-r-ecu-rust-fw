110a168baae7ac46e73e8665d79ed6cf344a3f13
Alan Rodriguez <alan.9489@gmail.com>
2025-11-04 15:00:40 -0600

    Se cambio el proyecto para usar rtt

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,83 @@
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_main]
 #![no_std]
-#![deny(warnings)]
-#![deny(unsafe_code)]
-#![deny(missing_docs)]
 
+use rtic::app;
+use rtic_monotonics::systick::prelude::*;
+use rtt_target::{rprintln, rtt_init_print};
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c    atch panics
 extern crate cortex_m_rt;
 extern crate cortex_m;
+//extern crate stm32l4;
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use stm32h7xx_hal::{
+    pac, prelude::*, gpio::PE3, gpio::Output, gpio::PushPull
+};
 
-#[rtic::app(device = lm3s6965, dispatchers = [UART0, UART1])]
+//use cortex_m::asm;
+//use cortex_m_rt::entry;
+
+systick_monotonic!(Mono, 1000);
+
+#[app(device = pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
+    use super::*;
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        local_to_foo: i64,
-        local_to_bar: i64,
-        local_to_idle: i64,
+        led: PE3<Output<PushPull>>,
+        state: bool,
     }
 
-    // `#[init]` cannot access locals from the `#[local]` struct as they are initialized here.
     #[init]
-    fn init(_: init::Context) -> (Shared, Local) {
-        foo::spawn().unwrap();
-        bar::spawn().unwrap();
+    fn init(cx: init::Context) -> (Shared, Local) {
+        // Setup clocks
+        let mut _flash = cx.device.FLASH;
 
-        (
-            Shared {},
-            // initial values for the `#[local]` resources
-            Local {
-                local_to_foo: 0,
-                local_to_bar: 0,
-                local_to_idle: 0,
-            },
-        )
+        // Initialize the systick interrupt & obtain the token to prove that we did
+        Mono::start(cx.core.SYST, 100_000_000); // default STM32F303 clock-rate is 36MHz
+
+        rtt_init_print!();
+        rprintln!("init");
+
+        let pwr = cx.device.PWR.constrain();
+        let pwrcfg = pwr.freeze();
+
+        let rcc = cx.device.RCC.constrain();
+        let ccdr = rcc
+        .use_hse(25.MHz())
+        .sys_ck(100.MHz())
+        .freeze(pwrcfg, &cx.device.SYSCFG);
+
+        // Setup LED
+        let gpioe = cx.device.GPIOE.split(ccdr.peripheral.GPIOE);
+
+        // Configure PE1 as output.
+        let led = gpioe.pe3.into_push_pull_output();
+
+        // Schedule the blinking task
+        blink::spawn().ok();
+
+        (Shared {}, Local { led, state: false })
     }
 
-    // `local_to_idle` can only be accessed from this context
-    #[idle(local = [local_to_idle])]
-    fn idle(cx: idle::Context) -> ! {
-        let local_to_idle = cx.local.local_to_idle;
-        *local_to_idle += 1;
-
-        hprintln!("idle: local_to_idle = {}", local_to_idle);
-
-        debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
-
-        // error: no `local_to_foo` field in `idle::LocalResources`
-        // _cx.local.local_to_foo += 1;
-
-        // error: no `local_to_bar` field in `idle::LocalResources`
-        // _cx.local.local_to_bar += 1;
-
+    #[task(local = [led, state])]
+    async fn blink(cx: blink::Context) {
         loop {
-            cortex_m::asm::nop();
+            rprintln!("blink");
+            if *cx.local.state {
+                cx.local.led.set_high();
+                *cx.local.state = false;
+            } else {
+                cx.local.led.set_low();
+                *cx.local.state = true;
+            }
+            Mono::delay(500.millis()).await;
         }
-    }
-
-    // `local_to_foo` can only be accessed from this context
-    #[task(local = [local_to_foo], priority = 1)]
-    async fn foo(cx: foo::Context) {
-        let local_to_foo = cx.local.local_to_foo;
-        *local_to_foo += 1;
-
-        // error: no `local_to_bar` field in `foo::LocalResources`
-        // cx.local.local_to_bar += 1;
-
-        hprintln!("foo: local_to_foo = {}", local_to_foo);
-    }
-
-    // `local_to_bar` can only be accessed from this context
-    #[task(local = [local_to_bar], priority = 1)]
-    async fn bar(cx: bar::Context) {
-        let local_to_bar = cx.local.local_to_bar;
-        *local_to_bar += 1;
-
-        // error: no `local_to_foo` field in `bar::LocalResources`
-        // cx.local.local_to_foo += 1;
-
-        hprintln!("bar: local_to_bar = {}", local_to_bar);
     }
 }
